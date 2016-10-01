1e6998b6e8a1217b142d1bf5e4e5e4ac758a59bd
Cortlandt Winters <cort@cortwinters.net>
2016-10-01 12:34:39 -0400

    add test files to the other domain classes

---
+++ test/match.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Assert = require('assert');
+const Lab = require('lab');
+
+const Match = require('../domain/match');
+
+const lab = exports.lab = Lab.script();
+
+lab.experiment('creates a new match', () => {
+
+  lab.test('- blank test', (done) => {
+    done();
+  });
+});
---
+++ test/player.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Assert = require('assert');
+const Lab = require('lab');
+
+const Player = require('../domain/player');
+
+const lab = exports.lab = Lab.script();
+
+lab.experiment('creates a player', () => {
+
+  lab.test('- blank test', (done) => {
+    done();
+  });
+});
