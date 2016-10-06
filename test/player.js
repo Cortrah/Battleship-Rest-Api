aff59b384c6d0c65ee70afb1651ca72c25b1ca2e
Cortlandt Winters <cort@cortwinters.net>
2016-10-06 12:28:12 -0400

    Add a basic test

--- test/player.js
+++ test/player.js
@@ -9,7 +9,14 @@ const lab = exports.lab = Lab.script();
 
 lab.experiment('creates a player', () => {
 
-  lab.test('- blank test', (done) => {
+  lab.test('- with defaults', (done) => {
+    let testPlayer = new Player('Rowlf')
+
+    Assert(testPlayer.name === 'Rowlf');
+    Assert(testPlayer.ip === 'http://pumpkin.local');
+    Assert(testPlayer.port === '4000');
+    Assert(testPlayer.wins === 0);
+    Assert(testPlayer.losses === 0);
     done();
   });
 });
