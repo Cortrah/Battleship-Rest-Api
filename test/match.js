e76a9d213654812654b1c3bcf4af66c303bca0db
Cortlandt Winters <cort@cortwinters.net>
2016-10-06 12:29:11 -0400

    add a pair of basic tests to create a match run it and that it increments wins and losses of the players

--- test/match.js
+++ test/match.js
@@ -2,14 +2,41 @@
 
 const Assert = require('assert');
 const Lab = require('lab');
-
+const Player = require('../domain/player');
 const Match = require('../domain/match');
 
 const lab = exports.lab = Lab.script();
 
 lab.experiment('creates a new match', () => {
 
-  lab.test('- blank test', (done) => {
+  let player1 = new Player("Guy");
+  let player2 = new Player("Smiley");
+  let testMatch = new Match(player1, player2);
+
+  lab.test('- it has two players', (done) => {
+
+    Assert(testMatch.players.length ===2);
     done();
   });
+
+  lab.test('- after running the play function there is a winner', (done) => {
+
+    testMatch.play();
+    Assert(testMatch.winner !== null);
+    done();
+  });
+
+  lab.test('- and it increments wins and losses', (done) => {
+
+    if(player1.id === testMatch.winner.id){
+      Assert(player1.wins === 1);
+      Assert(player2.losses === 1);
+    } else {
+      Assert(player2.wins === 1);
+      Assert(player1.losses === 1);
+    }
+    done();
+  });
+
+
 });
